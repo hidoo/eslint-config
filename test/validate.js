84681e9decc13b4859b7eaf3c960975f7fbb4c92
hidoo <hidoooo@gmail.com>
2019-06-18 20:14:50 +0900

    test(validate): fix arguments order of validator

--- test/validate.js
+++ test/validate.js
@@ -8,6 +8,6 @@ const validator = require('eslint/lib/config/config-validator');
  */
 const base = require('../lib/base');
 
-validator.validate(base, 'lib/base.js', () => {});
+validator.validate(base, () => {}, 'lib/base.js');
 
 console.log('ok'); // eslint-disable-line  no-console
