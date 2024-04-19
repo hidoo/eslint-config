6dc4fd503d32ada5854f32d5b75d07b12a541899
hidoo <4569054+hidoo@users.noreply.github.com>
2024-04-19 23:09:47 +0900

    fix!: remove eslint-plugin-eslint-comments
    Note: Use instead linterOptions.reportUnusedDisableDirectives.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ module.exports = {
   parserOptions,
   extends: [
     path.resolve(__dirname, 'lib', 'base.js'),
-    path.resolve(__dirname, 'lib', 'eslint-comments.js'),
     path.resolve(__dirname, 'lib', 'import.js'),
     path.resolve(__dirname, 'lib', 'jsdoc.js'),
     path.resolve(__dirname, 'lib', 'sort-class-members.js')
